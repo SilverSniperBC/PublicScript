de0c63d408a36a1d197e97f35a1fa0e96d37522b
Exploiter <43097389+AnLiterallyStupidPerson@users.noreply.github.com>
2022-10-22 22:21:02 +0200

    Update purpletreefinder.lua

--- purpletreefinder.lua
+++ purpletreefinder.lua
@@ -7,7 +7,7 @@ BillboardGui.AlwaysOnTop = true
 BillboardGui.LightInfluence = 1
 BillboardGui.Size = UDim2.new(0, 50, 0, 50)
 BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
-
+game.Players.LocalPlayer.Character:MoveTo(v.CFrame)
 TextLabel.Parent = BillboardGui
 TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
 TextLabel.BackgroundTransparency = 1
